a097ff64ff92cf6c9c10de67a203b59b1584a560
Muhammad Huzaifa <154605966+MuhammadHuzaifa9@users.noreply.github.com>
2024-02-28 15:09:21 +0500

    Add files via upload

---
+++ chapter1/app.js
@@ -0,0 +1,15 @@
+//Question1//
+window.alert('Hello!');
+//Question2//
+window.alert('Error!Please Enter a Valid Password.');
+//Question3//
+window.alert('Welcome to js land! \n Happy Coding.');
+//Question4//
+window.alert('Welcome to JS land!')
+window.alert('Happy Coding..')
+//Question5//
+console.log('Hello... I can run JS through my web browsers console.');
+//Question6//
+function showAlert(){
+    alert('Im a Button');
+}
