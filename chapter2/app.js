76aeaa15044b0d754ae2f0b625cfad32257a3573
Muhammad Huzaifa <154605966+MuhammadHuzaifa9@users.noreply.github.com>
2024-02-28 16:27:05 +0500

    Add files via upload

---
+++ chapter2/app.js
@@ -0,0 +1,28 @@
+var username;
+var myName='Muhammad Huzaifa';
+//Question3//
+var message='Hello World';
+window.alert(message);
+//Question4//
+var studentName='Muhammad Huzaifa';
+var studentAge=21;
+var studentField='Certified Mobile Appliction Development';
+window.alert(studentName);
+window.alert(studentAge);
+window.alert(studentField);
+//Question5//
+var message= 'pizza\npizz\npiz\npi\np'
+window.alert(message)
+//Question6//
+var email='huzaifaashfaq22@gmail.com'
+window.alert('My email address is '+ email);
+//Question7//
+var book='A smarter way to learn JavaScript';
+window.alert('I am trying to learn from the Book '+ book);
+//Question8//
+document.write('   Yah! I can write HTML content through JavaScript');
+//Question9//
+document.write('“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”');
+window.alert('“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”');
+
+
