289b58bf72e8de090d66301f0fbd1e841442df6b
Muhammad Huzaifa <154605966+MuhammadHuzaifa9@users.noreply.github.com>
2024-02-28 17:10:43 +0500

    Add files via upload

---
+++ chapter3/app.js
@@ -0,0 +1,18 @@
+//Question1//
+var age=21;
+window.alert('I am '+ age +' years old.');
+//Question2//
+var visits=14;
+window.alert('You have visited this website ' +visits+ ' times');
+//Question3//
+var birthYear=2002;
+document.write('My birth year is '+birthYear+  '<br/>Datatype of my declared variable is number<br/>');
+//Question4//
+var visitorsName='John Doe';
+var productTitle=' T-Shirt(s)';
+var Quantity=5;
+document.write('<br/>'+visitorsName+' ordered '+Quantity+productTitle+' on XYZClothing Store.');
+
+
+
+
